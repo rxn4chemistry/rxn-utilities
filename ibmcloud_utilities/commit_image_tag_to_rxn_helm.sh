1d43cfa3a55d0cf2badbf3be8eac2780d7604ab4
Aleksandros Sobczyk <obc@zurich.ibm.com>
2020-04-15 15:48:58 +0200

    Made script more verbose and added RXN_CLUSTER_NAME (might not necessarily be the same with GIT_BRANCH)

--- ibmcloud_utilities/commit_image_tag_to_rxn_helm.sh
+++ ibmcloud_utilities/commit_image_tag_to_rxn_helm.sh
@@ -2,16 +2,20 @@
 
 # Required env variables:
 # GHE_TOKEN
+# RXN_CLUSTER_NAME
 # RXN_FID_EMAIL
 
-git clone -q \
-          https://${GHE_TOKEN}@github.ibm.com/rxn/rxn-helm.git \
+git clone https://${GHE_TOKEN}@github.ibm.com/rxn/rxn-helm.git \
           ${TMPDIR}/rxn-helm
 
 cd ${TMPDIR}/rxn-helm/charts/
 
-VALUES_FILENAME="values_${GIT_BRANCH}.yaml"
+VALUES_FILENAME="values_${RXN_CLUSTER_NAME}.yaml"
 VALUES_FILES="$(find . -name ${VALUES_FILENAME} | grep ${IMAGE_NAME})"
+
+echo "Deploying to: ${RXN_CLUSTER_NAME}"
+echo "Found the corresponding files to edit:"
+echo "${VALUES_FILES[@]}"
 
 for VFILE in ${VALUES_FILES[@]};
 do
@@ -19,6 +23,7 @@ do
   git add ${VFILE}
 done
 
+echo "Committing changes and pushing to rxn-helm"
 git config user.email "${RXN_FID_EMAIL}"
 git config user.name "RXN Functional ID"
 git commit -m "IBM cloud delivery pipeline ${IDS_PROJECT_NAME}: Updated image tag to ${IMAGE_TAG} in ${VALUES_FILES[@]}"
