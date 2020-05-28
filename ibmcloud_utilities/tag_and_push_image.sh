be4002bf79ed4c85285c2b55275bf78a5863d693
Matteo Manica <tte@zurich.ibm.com>
2020-05-28 17:48:42 +0200

    feat: handling namepsace when tagging.
    Signed-off-by: Matteo Manica <tte@zurich.ibm.com>

--- ibmcloud_utilities/tag_and_push_image.sh
+++ ibmcloud_utilities/tag_and_push_image.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+get_namespace() {
+    environment=$1
+    if [[ "${environment}" == "roborxn_dev" ]]; then
+    echo "roborxn-dev"
+    elif [[ "${environment}" == "test" ]]; then
+    echo "rxn-test"
+    elif [[ "${environment}" == "production" ]]; then
+    echo "ibm-react"
+    fi
+}
+export -f get_namespace
+
+export REGISTRY_NAMESPACE=$(get_namespace ${GIT_BRANCH})
 ibmcloud login -a cloud.ibm.com -r eu-gb --apikey ${IBM_CLOUD_API_KEY}
 ibmcloud cr login
 docker tag ${IMAGE_NAME}:build ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
