2bc8de323f90286ce38dce1e549dc19d6dca3ba6
Aleksandros Sobczyk <obc@zurich.ibm.com>
2020-05-28 14:52:02 +0200

    ibmcloud login before push

--- ibmcloud_utilities/tag_and_push_image.sh
+++ ibmcloud_utilities/tag_and_push_image.sh
@@ -3,10 +3,11 @@
 # Input env variables
 # IMAGE_NAME
 # TRAVIS_BRANCH
-#
+# IBM_CLOUD_API_KEY
 
 TIMESTAMP=$( date -u "+%Y%m%d%H%M%S")
 IMAGE_TAG=${TRAVIS_BRANCH}-${TIMESTAMP}
 
+ibmcloud login -a cloud.ibm.com -r eu-gb --apikey ${IBM_CLOUD_API_KEY}
 docker tag ${IMAGE_NAME}:build ${IMAGE_NAME}:${IMAGE_TAG}
 docker push ${IMAGE_NAME}:${IMAGE_TAG}
