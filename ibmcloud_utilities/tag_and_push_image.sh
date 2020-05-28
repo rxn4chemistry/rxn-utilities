d2798f0cc6cbc4ab8f7ffbbc9b599ed4e934bda1
Matteo Manica <tte@zurich.ibm.com>
2020-05-28 17:46:02 +0200

    fix: adjusting variable names.
    Signed-off-by: Matteo Manica <tte@zurich.ibm.com>

--- ibmcloud_utilities/tag_and_push_image.sh
+++ ibmcloud_utilities/tag_and_push_image.sh
@@ -2,5 +2,5 @@
 
 ibmcloud login -a cloud.ibm.com -r eu-gb --apikey ${IBM_CLOUD_API_KEY}
 ibmcloud cr login
-docker tag ${IMAGE_NAME}:build ${REGISTRY_URL}/${NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
-docker push ${REGISTRY_URL}/${NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
+docker tag ${IMAGE_NAME}:build ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}
