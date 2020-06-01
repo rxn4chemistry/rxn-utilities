2be3ccdd10660e841ef55e33e6f075c1f477dbe5
Matteo Manica <tte@zurich.ibm.com>
2020-06-01 14:24:09 +0200

    fix: updating url for ibmcloud installer.
    Signed-off-by: Matteo Manica <tte@zurich.ibm.com>

--- ibmcloud_utilities/install_ibmcloud.sh
+++ ibmcloud_utilities/install_ibmcloud.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -sL https://ibm.biz/idt-installer | bash
+curl -sL https://raw.githubusercontent.com/IBM-Cloud/ibm-cloud-developer-tools/master/linux-installer/idt-installer | bash
 ibmcloud --version
+
