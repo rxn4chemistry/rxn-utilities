51ba5f70c028ccab74afb944aca71287045b8f31
Alain Claude Vaucher <AVA@zurich.ibm.com>
2020-07-03 09:13:38 +0200

    Create class RxnEnum (#24)

---
+++ rxn_utilities/rxn_types.py
@@ -0,0 +1,50 @@
+"""Custom types used in RXN projects."""
+from enum import Enum
+
+
+class RxnEnum(Enum):
+    """
+    Custom enum with additional functions for string conversion.
+
+    Has the following functionality compared to a standard Enum:
+    * to_string() to generate a lowercase representation of the instance.
+    * from_string(value) to instantiate from a string.
+    * Constructor is valid both with a string and with another enum instance.
+    """
+
+    def to_string(self) -> str:
+        """
+        Convert the enum to a string representation (all lowercase).
+        """
+        return self.name.lower()
+
+    @classmethod
+    def from_string(cls, value: str) -> 'RxnEnum':
+        """
+        Construct the enum from a string, i.e. from the the strings of its
+        possible values.
+
+        Args:
+            value: string to convert to an instance of the Enum.
+
+        Raises:
+            ValueError if the value is not found.
+
+        Returns:
+            An instance of the Enum.
+        """
+        try:
+            return cls[value.upper()]
+        except KeyError as e:
+            allowed_values = [v.to_string() for v in cls]
+            allowed_values_str = ", ".join(allowed_values)
+            raise ValueError(
+                f'Invalid value: "{value}". Only the following are allowed: {allowed_values_str}.'
+            ) from e
+
+    @classmethod
+    def _missing_(cls, value) -> 'RxnEnum':
+        """
+        Overriden to allow instantiation from both string and enum value.
+        """
+        return cls.from_string(value)
---
+++ rxn_utilities/test/test_rxn_types.py
@@ -0,0 +1,39 @@
+from enum import auto
+
+import pytest
+
+from rxn_utilities.rxn_types import RxnEnum
+
+
+class DummyEnum(RxnEnum):
+    """
+    Concrete Enum for testing RxnEnum.
+    """
+    ITALY = auto()
+    SWITZERLAND = auto()
+    GERMANY = auto()
+
+
+def test_rxn_enum():
+    # Instantiation from enum
+    assert DummyEnum(DummyEnum.ITALY) == DummyEnum.ITALY
+    assert DummyEnum(DummyEnum.GERMANY) == DummyEnum.GERMANY
+
+    # Instantiation from string
+    assert DummyEnum('italy') == DummyEnum.ITALY
+    assert DummyEnum('Italy') == DummyEnum.ITALY
+    assert DummyEnum('ITALY') == DummyEnum.ITALY
+
+    # from_string
+    assert DummyEnum.from_string('italy') == DummyEnum.ITALY
+    assert DummyEnum.from_string('GERMANY') == DummyEnum.GERMANY
+
+    # to_string
+    assert DummyEnum.ITALY.to_string() == 'italy'
+
+    # exceptions
+    with pytest.raises(ValueError) as exc_info:
+        DummyEnum('zurich')
+    expected_exception_string = 'Invalid value: "zurich". Only the following are allowed: ' \
+                                'italy, switzerland, germany.'
+    assert expected_exception_string in str(exc_info)
