5737af922c56e4d4c494388145e8c23610811c84
Alain Vaucher <AVA@zurich.ibm.com>
2021-11-24 17:38:53 +0100

    Script for stable shuffling (#66)
    * initial files
    
    * finalize
    
    * doc update

---
+++ rxn_utilities/scripts/stable_shuffle.py
@@ -0,0 +1,26 @@
+import random
+
+import click
+
+from rxn_utilities.file_utilities import load_list_from_file, dump_list_to_file
+
+
+@click.command()
+@click.argument('input_file', type=str, required=True)
+@click.argument('output_file', type=str, required=True)
+@click.option('--seed', type=int, default=42, help='Random seed')
+def main(input_file: str, output_file: str, seed: int):
+    """Shuffle a file in a deterministic order (the same seed always reorders
+    files of the same number of lines identically).
+
+    Useful, as an example, to shuffle both source and target files identically.
+    """
+    random.seed(seed)
+
+    lines = load_list_from_file(input_file)
+    random.shuffle(lines)
+    dump_list_to_file(lines, output_file)
+
+
+if __name__ == '__main__':
+    main()
