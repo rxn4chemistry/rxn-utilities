7e4c1ee90eb1e962b37c36d26bef56e6479a85af
Alain Vaucher <AVA@zurich.ibm.com>
2022-05-12 12:42:39 +0200

    Add utility functions for setting up loggers (#82) [bump patch]

--- rxn_utilities/logging_utilities.py
+++ rxn_utilities/logging_utilities.py
@@ -4,10 +4,92 @@
 # ALL RIGHTS RESERVED
 
 import logging
-from typing import Union
+from enum import Enum
+from typing import Union, Iterable
+
+from rxn_utilities.file_utilities import PathLike
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+
+
+class LoggingFormat(Enum):
+    """
+    Common logging formats used in the RXN universe.
+    """
+    BASIC = "[%(asctime)s %(levelname)s] %(message)s"
+    DETAILED = '%(asctime)s %(levelname)-7s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
+
+
+def setup_console_logger(
+    level: Union[int, str] = 'INFO',
+    format: Union[LoggingFormat, str] = LoggingFormat.BASIC
+) -> None:
+    """
+    Set up a logger writing to the console (i.e., to stderr).
+
+    Args:
+        level: log level, either as a string ("INFO") or integer (logging.INFO).
+        format: log format, as a LoggingFormat value, or a string directly.
+    """
+    _setup_logger_from_handlers(handlers=[logging.StreamHandler()], level=level, format=format)
+
+
+def setup_file_logger(
+    filename: PathLike,
+    level: Union[int, str] = 'INFO',
+    format: Union[LoggingFormat, str] = LoggingFormat.BASIC
+) -> None:
+    """
+    Set up a logger writing to the given file.
+
+    Overwrites the default file mode 'a' with 'w' (i.e., overwrites the file).
+
+    Args:
+        level: log level, either as a string ("INFO") or integer (logging.INFO).
+        format: log format, as a LoggingFormat value, or a string directly.
+    """
+    _setup_logger_from_handlers(
+        handlers=[logging.FileHandler(filename, mode='w')], level=level, format=format
+    )
+
+
+def setup_console_and_file_logger(
+    filename: PathLike,
+    level: Union[int, str] = 'INFO',
+    format: Union[LoggingFormat, str] = LoggingFormat.BASIC
+) -> None:
+    """
+    Set up a logger writing to both the terminal and the given file.
+
+    Overwrites the default file mode 'a' with 'w' (i.e., overwrites the file).
+
+    Args:
+        level: log level, either as a string ("INFO") or integer (logging.INFO).
+        format: log format, as a LoggingFormat value, or a string directly.
+    """
+    _setup_logger_from_handlers(
+        handlers=[logging.FileHandler(filename, mode='w'),
+                  logging.StreamHandler()],
+        level=level,
+        format=format
+    )
+
+
+def _setup_logger_from_handlers(
+    handlers: Iterable[logging.Handler], level: Union[int, str], format: Union[LoggingFormat, str]
+) -> None:
+    """
+    Helper function to avoid duplication in the other setup functions.
+
+    Args:
+        handlers: log handlers.
+        level: log level, either as a string ("INFO") or integer (logging.INFO).
+        format: log format, as a LoggingFormat value, or a string directly.
+    """
+    if isinstance(format, LoggingFormat):
+        format = format.value
+    logging.basicConfig(format=format, level=level, handlers=handlers)
 
 
 def setup_celery_logger(
