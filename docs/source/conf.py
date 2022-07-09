0f8977a77bfdfafc71783501ae215f2c57dc3393
Alain Vaucher <acvaucher@gmail.com>
2022-07-09 18:11:37 +0200

    Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,3 @@
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -9,43 +8,44 @@
 
 import pathlib
 import sys
+
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'RXN utilities'
+project = "RXN utilities"
 author = "IBM RXN team"
 copyright = "IBM RXN team 2022"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.autodoc',
+    "sphinx.ext.duration",
+    "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.githubpages",
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    'sphinx_autodoc_typehints',
-    'myst_parser',
+    "sphinx_autodoc_typehints",
+    "myst_parser",
 ]
 
 napoleon_google_docstring = True
-autodoc_typehints = 'both'
+autodoc_typehints = "both"
 always_document_param_types = True
-typehints_defaults = 'comma'
+typehints_defaults = "comma"
 
 # Note: see https://stackoverflow.com/a/62613202
 autosummary_generate = True
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -58,9 +58,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
