c359ff3a7005727c4508ba74d4196f29e0ef727b
Matteo Manica <drugilsberg@gmail.com>
2022-07-20 16:00:22 +0200

    feat: addressing PR comments from avaucher.
    Sorting deps in setup.cfg.
    
    Skipping tests based on environment variables.
    
    Signed-off-by: Matteo Manica <drugilsberg@gmail.com>

--- tests/test_databases_pymongo.py
+++ tests/test_databases_pymongo.py
@@ -6,13 +6,29 @@ import pytest
 from rxn.utilities.databases.pymongo import PyMongoSettings
 
 
+def _instantiate_client_and_check_server_info():
+    pymongo_settings = PyMongoSettings()
+    client = pymongo_settings.get_client()
+    server_info = client.server_info()
+    assert isinstance(server_info, dict)
+    assert "version" in server_info
+
+
 @pytest.fixture
 def mock_no_mongo_uri_env(monkeypatch):
+    """Delete environment variable RXN_MONGO_URI.
+
+    Needed to simulate environments with no MongoDB settings.
+    """
     monkeypatch.delenv("RXN_MONGO_URI", raising=False)
 
 
 @pytest.fixture
 def mock_mongo_certificate_env(monkeypatch):
+    """If a certificate is provided as an environment variable store it in a file and configure the environment.
+
+    Needed to simulate environments with MongoDB TLS connections.
+    """
     if "RXN_TLS_CA_CERTIFICATE" in os.environ:
         directory = tempfile.mkdtemp(prefix="rxn-utilities-pymongo")
         tls_ca_cert_filepath = os.path.join(directory, "ca.cert")
@@ -29,28 +45,18 @@ def test_get_pymongo_settings(mock_mongo_certificate_env):
     )
 
 
+@pytest.mark.skipif(
+    "RXN_MONGO_URI" not in os.environ, reason="environment_not_configured_for_test"
+)
 def test_get_client():
-    pymongo_settings = PyMongoSettings()
-    if "RXN_MONGO_URI" in os.environ:
-        client = pymongo_settings.get_client()
-        server_info = client.server_info()
-        assert isinstance(server_info, dict)
-        assert "version" in server_info
-    else:
-        with pytest.raises(ValueError):
-            _ = pymongo_settings.get_client()
+    _instantiate_client_and_check_server_info()
 
 
+@pytest.mark.skipif(
+    "RXN_MONGO_URI" not in os.environ, reason="environment_not_configured_for_test"
+)
 def test_get_client_with_tls_ca_cert(mock_mongo_certificate_env):
-    pymongo_settings = PyMongoSettings()
-    if "RXN_MONGO_URI" in os.environ:
-        client = pymongo_settings.get_client()
-        server_info = client.server_info()
-        assert isinstance(server_info, dict)
-        assert "version" in server_info
-    else:
-        with pytest.raises(ValueError):
-            _ = pymongo_settings.get_client()
+    _instantiate_client_and_check_server_info()
 
 
 def test_get_pymongo_settings_with_no_mongo_uri(mock_no_mongo_uri_env):
